f218bdab1c5a8bb88c158aeb670aabb4754f34ca
aserdar <serdarakkol@gmail.com>
2020-10-16 23:05:09 -0400

    removed clutter

--- check_event_length.m
+++
@@ -1,7 +0,0 @@
-%% CHECK THE LENGTHS OF EVENTS
-% Come to iEEG_data folder of the subject and then run this
-allecog = dir(fullfile(pwd,'*','*_ecog.mat'));
-for i=1:length(allecog)
-    ecogss = load(fullfile(allecog(i).folder,allecog(i).name));
-    fprintf('There are %d events in %s.\n',size(ecogss.ecog.events.eventsSample,2),allecog(i).name)
-end
